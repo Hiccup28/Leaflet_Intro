d5f460aec9a543e32cdb248a298bad43264df4fb
Hiccup28 <post2veera@gmail.com>
2025-01-21 17:29:41 +0530

    Set_2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([13.120214088073864, 80.23700235529519], 14);
+var map = L.map('map1').setView([13.120214088073864, 80.23700235529519], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -10,3 +10,6 @@ var marker = L.marker([13.120214088073864, 80.23700235529519]).addTo(map)
         var marker2= L.marker([13.082462755140616, 80.27549384469123]).addTo(map)
 		.bindPopup('Work_Location')
 		.openPopup();        
+        var marker3= L.marker([12.082462755140616, 79.27549384469123]).addTo(map)
+		.bindPopup('Work_Location2')
+		.openPopup();  
